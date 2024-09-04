d127ea5cd4bc1564a233cfe7864dc3d4cc8b46e4
Andrew McGivery <andrew.mcgivery@apollographql.com>
2024-09-04 01:05:51 -0400

    Add a missing test, instrument function calls, variable declarations, and throws, beginning to instrument branches

--- src/extensions/file_coverage.rs
+++ src/extensions/file_coverage.rs
@@ -1,19 +1,10 @@
-use crate::extensions;
-use ascii_table::{Align, AsciiTable};
 use colored::*;
 use regex::Regex;
-use rhai::{
-    module_resolvers::FileModuleResolver, ASTNode, Engine, EvalAltResult, Expr, Module,
-    ModuleResolver, Position, Scope, Stmt,
-};
+use rhai::{Engine, EvalAltResult, Module, ModuleResolver, Position, Scope};
 use std::{
     collections::HashMap,
-    fmt::{format, Display},
     fs,
-    hash::Hash,
-    i64,
     path::{Path, PathBuf},
-    rc::Rc,
     sync::{Arc, Mutex},
 };
 use tabled::{settings::Style, Table, Tabled};
@@ -55,10 +46,10 @@ impl ModuleResolver for FileCoverageModuleResolver {
     // Only required function.
     fn resolve(
         &self,
-        engine: &Engine,      // reference to the current 'Engine'
-        source: Option<&str>, // path of the parent module
-        path: &str,           // the module path
-        pos: Position,        // position of the 'import' statement
+        engine: &Engine,
+        source: Option<&str>,
+        path: &str,
+        pos: Position,
     ) -> Result<rhai::Shared<rhai::Module>, Box<rhai::EvalAltResult>> {
         let global = &mut engine.new_global_runtime_state();
         let scope = &mut Scope::new();
@@ -71,26 +62,14 @@ impl ModuleResolver for FileCoverageModuleResolver {
         let mut contents = fs::read_to_string(file_path)
             .map_err(|_| Box::new(EvalAltResult::ErrorModuleNotFound(path.to_string(), pos)))?;
 
-        /*
-        println!("*****");
-        println!("{}", path);
-        println!(
-            "{}",
-            contents
-                .lines()
-                .enumerate()
-                .map(|(i, line)| { instrument_line(i, line, path) })
-                .collect::<Vec<_>>()
-                .join("\n")
-        );
-        println!("*****");*/
-
         contents = contents
             .lines()
             .enumerate()
             .map(|(i, line)| instrument_line(i, line, path, self.test_coverage_container.clone()))
             .collect::<Vec<_>>()
             .join("\n");
+
+        //println!("{}", contents);
 
         let mut ast = engine.compile(&contents).map_err(|err| {
             Box::new(EvalAltResult::ErrorInModule(
@@ -121,17 +100,16 @@ fn instrument_line(
         .unwrap()
         .captures(line)
     {
+        // Instrument Functions
         if let Some(matched) = captures.get(1) {
             let function_name = matched.as_str();
 
-            // First, add the function to tracked functions
             test_coverage_container.lock().unwrap().add_function(
                 function_name.to_string(),
                 path.to_string(),
                 (i as i64) + 1,
             );
 
-            // Second, add the instrumentation
             let instrumentation = format!(
                 "rhai_test_coverage_instrument_function(\"{}\",\"{}\",{} );",
                 function_name,
@@ -140,6 +118,56 @@ fn instrument_line(
             );
             result = format!("{} {}", line, instrumentation);
         }
+    } else if (Regex::new(r#".+?\(.*?\);"#).unwrap().is_match(line)) {
+        // Function Call Statements
+        test_coverage_container
+            .lock()
+            .unwrap()
+            .add_statement(path.to_string(), (i as i64) + 1);
+
+        let instrumentation = format!(
+            "rhai_test_coverage_instrument_statement(\"{}\",{} );",
+            path,
+            (i as i64) + 1
+        );
+        result = format!("{} {}", line, instrumentation);
+    } else if (Regex::new(r#"(let )?.+?=.+?;"#).unwrap().is_match(line)) {
+        // Variable declarations
+        test_coverage_container
+            .lock()
+            .unwrap()
+            .add_statement(path.to_string(), (i as i64) + 1);
+
+        let instrumentation = format!(
+            "rhai_test_coverage_instrument_statement(\"{}\",{} );",
+            path,
+            (i as i64) + 1
+        );
+        result = format!("{} {}", line, instrumentation);
+    } else if (Regex::new(r#"(else|else if|if).+?\{"#)
+        .unwrap()
+        .is_match(line))
+    {
+        // Branches
+        let instrumentation = format!(
+            "rhai_test_coverage_instrument_branch(\"{}\",{} );",
+            path,
+            (i as i64) + 1
+        );
+        result = format!("{} {}", line, instrumentation);
+    } else if (Regex::new(r#"throw.+?\{"#).unwrap().is_match(line)) {
+        // throws
+        test_coverage_container
+            .lock()
+            .unwrap()
+            .add_statement(path.to_string(), (i as i64) + 1);
+
+        let instrumentation = format!(
+            "rhai_test_coverage_instrument_statement(\"{}\",{} );",
+            path,
+            (i as i64) + 1
+        );
+        result = format!("{} {}", instrumentation, line);
     }
     result
 }
@@ -147,6 +175,8 @@ fn instrument_line(
 #[derive(Debug)]
 struct TestCoverageSource {
     pub name: String,
+    statements: HashMap<String, StatementCoverage>,
+    branches: HashMap<String, BranchCoverage>,
     functions: HashMap<String, FunctionCoverage>,
 }
 
@@ -159,9 +189,21 @@ struct FunctionCoverage {
 }
 
 #[derive(Debug)]
+struct StatementCoverage {
+    pub source: String,
+    pub line_number: i64,
+    pub is_hit: bool,
+}
+
+#[derive(Debug)]
+struct BranchCoverage {
+    pub source: String,
+    pub line_number: i64,
+    pub is_hit: bool,
+}
+
+#[derive(Debug)]
 pub struct TestCoverageContainer {
-    // TODO: This should actually be stored by source so we can do a coverage report
-    //functions: HashMap<String, FunctionCoverage>,
     sources: HashMap<String, TestCoverageSource>,
 }
 
@@ -181,6 +223,8 @@ impl TestCoverageContainer {
                 TestCoverageSource {
                     name,
                     functions: HashMap::new(),
+                    statements: HashMap::new(),
+                    branches: HashMap::new(),
                 },
             );
         }
@@ -190,15 +234,33 @@ impl TestCoverageContainer {
         self.maybe_add_source(&source);
         let key = TestCoverageContainer::get_function_key(&function_name, &source, &line_number);
 
-        self.sources.get_mut(&source).unwrap().functions.insert(
-            key,
-            FunctionCoverage {
+        self.sources
+            .get_mut(&source)
+            .unwrap()
+            .functions
+            .entry(key)
+            .or_insert(FunctionCoverage {
                 function_name,
                 source,
                 line_number,
                 is_hit: false,
-            },
-        );
+            });
+    }
+
+    pub fn add_statement(&mut self, source: String, line_number: i64) {
+        self.maybe_add_source(&source);
+        let key = TestCoverageContainer::get_statement_key(&source, &line_number);
+
+        self.sources
+            .get_mut(&source)
+            .unwrap()
+            .statements
+            .entry(key)
+            .or_insert(StatementCoverage {
+                source,
+                line_number,
+                is_hit: false,
+            });
     }
 
     pub fn function_called(&mut self, function_name: String, source: String, line_number: i64) {
@@ -213,8 +275,24 @@ impl TestCoverageContainer {
             .is_hit = true;
     }
 
+    pub fn statement_called(&mut self, source: String, line_number: i64) {
+        let key = TestCoverageContainer::get_statement_key(&source, &line_number);
+
+        self.sources
+            .get_mut(&source)
+            .unwrap()
+            .statements
+            .get_mut(&key)
+            .unwrap()
+            .is_hit = true;
+    }
+
     fn get_function_key(function_name: &String, source: &String, line_number: &i64) -> String {
         format!("{}-{}-{}", function_name, source, line_number)
+    }
+
+    fn get_statement_key(source: &String, line_number: &i64) -> String {
+        format!("{}-{}", source, line_number)
     }
 
     pub fn print_results(&mut self) {
@@ -224,13 +302,13 @@ impl TestCoverageContainer {
 
         self.sources.iter().for_each(|(_, coverage_source)| {
             let source = &coverage_source.name;
-            let total_functions = coverage_source.functions.len();
-            let hit_functions = coverage_source
-                .functions
-                .iter()
-                .filter(|(_, function)| function.is_hit)
-                .count();
             let percent_functions = {
+                let total_functions = coverage_source.functions.len();
+                let hit_functions = coverage_source
+                    .functions
+                    .iter()
+                    .filter(|(_, function)| function.is_hit)
+                    .count();
                 let percent = (hit_functions as f64 / total_functions as f64) * 100.0;
                 if percent >= 80.0 {
                     percent.to_string().green()
@@ -240,14 +318,45 @@ impl TestCoverageContainer {
                     percent.to_string().red()
                 }
             };
+            let percent_statements = {
+                let total_statements = coverage_source.statements.len();
+                let hit_statements = coverage_source
+                    .statements
+                    .iter()
+                    .filter(|(_, statement)| statement.is_hit)
+                    .count();
+                println!(
+                    "total_statements: {}, hit_statements: {}",
+                    total_statements, hit_statements
+                );
+                println!(
+                    "coverage_source.statements: {:?}",
+                    coverage_source.statements
+                );
+                let percent = (hit_statements as f64 / total_statements as f64) * 100.0;
+                if percent >= 80.0 {
+                    percent.to_string().green()
+                } else if percent >= 50.0 {
+                    percent.to_string().yellow()
+                } else {
+                    percent.to_string().red()
+                }
+            };
+            let uncovered_lines = coverage_source
+                .statements
+                .iter()
+                .filter(|(_, statement)| !statement.is_hit)
+                .map(|(_, statement)| statement.line_number.to_string())
+                .collect::<Vec<_>>()
+                .join(",");
 
             report_data.push(CoverageReportLine {
                 source: source.to_string(),
-                statements: "0".to_string(),
+                statements: percent_statements.to_string(),
                 branches: "0".to_string(),
                 functions: percent_functions.to_string(),
                 lines: "0".to_string(),
-                uncovered_lines: "0".to_string(),
+                uncovered_lines: uncovered_lines.to_string(),
             });
         });
 
@@ -281,18 +390,46 @@ pub fn register_rhai_functions_and_types(
     engine: &mut Engine,
     test_coverage_container: Arc<Mutex<TestCoverageContainer>>,
 ) {
-    let test_coverage_container_clone = test_coverage_container.clone();
-
+    let test_coverage_container_functions_clone = test_coverage_container.clone();
     let rhai_test_coverage_instrument_function =
         move |function_name: String, source: String, line_number: i64| {
-            test_coverage_container_clone
+            test_coverage_container_functions_clone
                 .lock()
                 .unwrap()
                 .function_called(function_name, source, line_number);
         };
 
+    let test_coverage_container_statements_clone = test_coverage_container.clone();
+    let rhai_test_coverage_instrument_statement = move |source: String, line_number: i64| {
+        test_coverage_container_statements_clone
+            .lock()
+            .unwrap()
+            .statement_called(source, line_number);
+    };
+
+    let rhai_test_coverage_instrument_branch = move |source: String, line_number: i64| {
+        /*println!(
+            "rhai_test_coverage_instrument_statement: {} {}",
+            source, line_number
+        );*/
+        /*test_coverage_container_clone
+        .lock()
+        .unwrap()
+        .function_called(function_name, source, line_number);*/
+    };
+
     engine.register_fn(
         "rhai_test_coverage_instrument_function",
         rhai_test_coverage_instrument_function,
+    );
+
+    engine.register_fn(
+        "rhai_test_coverage_instrument_statement",
+        rhai_test_coverage_instrument_statement,
+    );
+
+    engine.register_fn(
+        "rhai_test_coverage_instrument_branch",
+        rhai_test_coverage_instrument_branch,
     );
 }
